9143189546302b05ddb619970f63cf214b0ebf43
emirandemir <demiremirhan1903@gmail.com>
2024-10-05 16:06:02 +0300

    Nlog kütüphanesi kullanılarak loglama işlemi projeye entegre edildi.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using NLog;
 using Repositories.EFCore;
+using Services;
 using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(),"/nlog.config"));
 // Add services to the container.
 
 builder.Services.AddControllers()
@@ -16,6 +19,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager(); 
+builder.Services.ConfigureLoggerService();
 
 var app = builder.Build();
 
