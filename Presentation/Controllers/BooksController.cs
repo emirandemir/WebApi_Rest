c0a7fa8ea524f641ed3edd5a319131ea6fabafec
emirandemir <demiremirhan1903@gmail.com>
2024-10-22 21:20:02 +0300

    Hata mesajlarını yazdırma merkezileştirildi ve projeye automapper eklendi

--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Models;
+﻿using Entities.DataTransferObjects;
+using Entities.Exceptions;
+using Entities.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Services.Contracts;
@@ -16,138 +18,87 @@ namespace Presentation.Controllers
 
     public class BooksController : ControllerBase
     {
-        
-            private readonly IServiceManager _manager;
 
-            public BooksController(IServiceManager manager)
-            {
-                _manager = manager;
-            }
+        private readonly IServiceManager _manager;
 
-            [HttpGet]
-            public IActionResult GetAllBooks()
-            {
+        public BooksController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
 
-                try
-                {
-                    var books = _manager.BookService.GetAllBooks(false);
-                    return Ok(books);
-                }
-
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-
-            }
-
-            [HttpGet("{id:int}")]
-            public IActionResult GetBook([FromRoute(Name = "id")] int id)
-            {
-
-                try
-                {
-                    var book = _manager.BookService.GetOneBookById(id, false);
-
-                    if (book == null)
-                    {
-                        return NotFound();
-                    }
-
-                    return Ok(book);
-                }
-
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-
-            }
-
-            [HttpPost]
-            public IActionResult CreateOneBook([FromBody] Book book)
-            {
-                try
-                {
-                    if (book == null)
-                    {
-                        return BadRequest();
-                    }
-
-                    _manager.BookService.CreateOneBook(book);
-
-                    return StatusCode(201, book);
-                }
-
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-
-            }
-
-            [HttpPut]
-            public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
-            {
-                try
-                {
-
-                    if (book is null)
-                        return BadRequest();
-
-                    _manager.BookService.UpdateOneBook(id, book, true);
-
-                    return NoContent();
-
-                }
-
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-            }
-
-            [HttpDelete("{id:int}")]
-            public IActionResult DeleteOneBook(int id)
-            {
-
-                try
-                {
-                    _manager.BookService.DeleteOneBook(id, false);
-                    return Ok();
-                }
-
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-
-            }
-
-            [HttpPatch]
-            public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<Book> bookPatch)
-            {
-                try
-                {
-
-                    var entity = _manager.BookService.GetOneBookById(id, true);
-
-                    if (entity is null)
-                        return NotFound();
-
-                    bookPatch.ApplyTo(entity);
-
-                    _manager.BookService.UpdateOneBook(id, entity, true);
-
-                    return NoContent();
-                }
-
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-            }
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _manager.BookService.GetAllBooks(false);
+            return Ok(books);
 
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetBook([FromRoute(Name = "id")] int id)
+        {
+
+            var book = _manager.BookService.GetOneBookById(id, false);
+
+            return Ok(book);
+
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            _manager.BookService.CreateOneBook(book);
+
+            return StatusCode(201, book);
+
+
+        }
+
+        [HttpPut]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] BookDtoForUpdate bookDto)
+        {
+
+
+            if (bookDto is null)
+                return BadRequest();
+
+            _manager.BookService.UpdateOneBook(id, bookDto, true);
+
+            return NoContent();
+
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook(int id)
+        {
+            _manager.BookService.DeleteOneBook(id, false);
+            return Ok();
+
+        }
+
+        [HttpPatch]
+        public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<Book> bookPatch)
+        {
+
+            var entity = _manager.BookService.GetOneBookById(id, true);
+
+            
+
+            bookPatch.ApplyTo(entity);
+
+            _manager.BookService.UpdateOneBook(id, new BookDtoForUpdate(entity.ID,entity.Title,entity.Price), true);
+
+            return NoContent();
+
+        }
+
     }
+}
 
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using NLog;
 using Repositories.EFCore;
 using Services;
+using Services.Contracts;
 using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,14 +21,24 @@ builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager(); 
 builder.Services.ConfigureLoggerService();
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
+
+
+var logger = app.Services.GetRequiredService<ILoggerService>();
+app.ConfigureExceptionHandler(logger);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+if (app.Environment.IsProduction())
+{
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
